f25f3c8fc1335a83cb7a383360580c8f0018219d
jonnyhak <jonnyhak@gmail.com>
2020-10-19 17:47:39 -0400

    pseudo code for bets and cards

---
+++ app/models/hackjack.rb
@@ -0,0 +1,44 @@
+require 'deck-of-cards'
+
+class HackJack
+
+    def self.place_your_bet
+        puts "Place your bet!"
+        bet_amount = gets.chomp
+        if bet_amount.to_i > 20 
+            puts "Bet must be lower than bank amount!"
+            self.place_your_bet
+        else 
+            "You bet #{bet_amount.to_i} on this round!"
+        end
+    end
+
+    def self.user_cards 
+        # deck = DeckOfCards.new
+        # deck.shuffle
+
+        # user_card_1 = deck.draw
+        # user_card_2 = deck.draw
+
+        # [user_card_1, user_card_2]
+        deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+        user_card_1 = deck.sample
+        user_card_2 = deck.sample
+
+        total = user_card_1 + user_card_2
+    end
+
+    def self.dealer_card
+        deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+        dealer_card_1 = deck.sample 
+    end
+
+
+
+
+
+
+
+end
--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,18 @@
 require_relative '../config/environment'
+require "tty-prompt"
+require 'deck-of-cards' 
 
 
+# prompt = TTY::Prompt.new
+
+# puts prompt.yes?("Do you like Ruby?")
+
+puts HackJack.place_your_bet
+puts HackJack.user_cards
+
+puts HackJack.dealer_card
 
 
-puts alexa 
+# puts "Welcome to HackJack" 
 
 # puts "HELLO WORLD"
